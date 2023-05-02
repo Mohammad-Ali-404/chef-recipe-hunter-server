6c94318b00a84805087bb58d932f31946dc66888
wpmohammad1 <alimohammadjhd@gmail.com>
2023-05-02 12:12:50 +0600

    create local server and loaded chefdata

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const express = require('express')
+const app = express();
+const port = 5000;
+const chefData = require('./data/ChefData.json')
+
+app.get('/', (req, res) =>{
+    res.send("port is running")
+})
+app.get('/chefdata', (req, res) =>{
+    res.send(chefData)
+})
+app.listen(port, ()=>{
+    console.log(`Chef Api port is running: ${port}`)
+})
